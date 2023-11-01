76fd8c0d52064daa0be06aeeb096e234c7a13ee8
German Cordova <german.sc937@gmail.com>
2023-11-01 17:08:40 -0500

    UpdateMain

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const metroAPI = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/",
+});
+
+// == LOGIN == //
+
+export const login = (userData) => {
+  return metroAPI.post("login", userData);
+};
