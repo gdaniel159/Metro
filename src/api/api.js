293820d7a0672f9f950ae523046985c174c3bf88
Andrea Aguirre <aguirreandrea457@gmail.com>
2023-11-02 16:08:02 -0500

    update

--- src/api/api.js
+++ src/api/api.js
@@ -30,6 +30,25 @@ export const getProducts = () => {
 
 // Structure //
 
+export const createCategories = (newCategoria) => {
+  return metroAPI.post(`categories/create`,newCategoria);
+}
+
+export const createEmployees = (newEmployees) => {
+  return metroAPI.post(`employees/create`,newEmployees);
+}
+
+export const createCustomers = (newCustomers) => {
+  return metroAPI.post(`customers/create`, newCustomers);
+}
+
+export const createOrders = (newOrders) => {
+  return metroAPI.post(`orders/create`, newOrders);
+}
+
+export const createProducts = (newProducts) => {
+  return metroAPI.post(`products/create`, newProducts);
+}
 /*
 
   export const storeSomething = (newSomething) => {
@@ -41,6 +60,26 @@ export const getProducts = () => {
 // == PUT == //
 
 // Structure //
+
+export const updateCategories = (id, updateCategories) => {
+  return metroAPI.put(`categories/udpate/${id}`,updateCategories);
+}
+
+export const updateEmployees = (id, updateEmployees) => {
+  return metroAPI.put(`employees/update/${id}`,updateEmployees);
+}
+
+export const updateCustomers = (id, updateCustomers) =>  {
+  return metroAPI.put(`customers/update/${id}`,updateCustomers);
+}
+
+export const updateOrders = (id, updateOrders) => {
+  return metroAPI.put(`orders/update/${id}`,updateOrders);
+}
+
+export const updateProducts = (id, updateProducts) => {
+  return metroAPI.put(`products/update/${id}`,updateProducts);
+}
 
 /*
 
@@ -54,6 +93,25 @@ export const getProducts = () => {
 
 // Structure //
 
+export const deleteCategories = (id) => {
+  return metroAPI.delete(`categories/delete/${id}`);
+}
+
+export const deleteEmployees = (id) => {
+  return metroAPI.delete(`employees/delete/${id}`);
+}
+
+export const deleteCustomers = (id) => {
+  return metroAPI.delete(`customers/delete/${id}`);
+}
+
+export const deleteOrders = (id) => {
+  return metroAPI.delete(`orders/delete/${id}`);
+}
+
+export const deleteProducts = (id) => {
+  return metroAPI.delete(`products/delete/${id}`);
+}
 /*
 
   export const deleteSomething = (id) => {
