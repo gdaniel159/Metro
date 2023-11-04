95700358fd4b8df029c2e441f652c688253ba2b9
German Cordova <german.sc937@gmail.com>
2023-11-04 16:40:07 -0500

    UpdateMain

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -184,13 +184,6 @@ export default function NavBar() {
                       />
                       <small id="password-help">Ingresa tu contraseña.</small>
                     </div>
-                    <div className="flex justify-content-end gap-2 mb-3">
-                      <small>
-                        <Link to="/register">
-                          ¿Aun no tienes cuenta?
-                        </Link>
-                      </small>
-                    </div>
                     <Button
                       label="Iniciar Sesion"
                       className="btn btn-primary mb-3 form-control"
